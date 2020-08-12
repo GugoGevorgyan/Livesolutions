545f7c1ab90099986357328dcab4c85ead32de93
GEVORG <gevor_grigoryan89@mail.ru>
2020-08-12 14:15:35 +0400

    change name in attach_product

--- app/Product.php
+++ app/Product.php
@@ -8,5 +8,4 @@ class Product extends Model
 {
     protected $guarded =[];
 
-
 }
---
+++ database/migrations/2020_08_12_101411_create_attach_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attach_products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned();
+            $table->bigInteger('company_id')->unsigned();
+            $table->float('count');
+            $table->float('price');
+            $table->float('discount');
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('profiles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attach_products');
+    }
+}
